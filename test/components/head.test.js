d081fcde726c5e55c0d6b755af1f028ed6958f0b
Yoshiki Aoki <ringohub@gmail.com>
2017-06-15 20:11:16 +0900

    Update

---
+++ test/components/head.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import test from 'ava';
+import Header from 'components/head';
+
+test('renders two `.Bar`', t => {
+  // const wrapper = shallow(<Foo/>);
+  const z = <Header/>;
+  t.is(true, true);
+});
